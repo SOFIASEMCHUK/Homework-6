012ab1467583f429a6f9016959692c0c4fdaaad0
SOFIASEMCHUK <semchuk.s02@gmail.com>
2022-12-13 19:01:39 +0200

    added js file

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const num1 = +prompt('Enter number one');
 const num2 = +prompt('Enter number two');
 const num3 = +prompt('Enter number tree');
 
-alert(`${(num1 + num2 + num3)/3}`);
+alert(`Середнє значення ${(num1 + num2 + num3)/3}`);
